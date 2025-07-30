541be450ea4efeccb9a3dc68191d1a66f84bca68
NaveenRPC <76088766+NaveenRPC@users.noreply.github.com>
2025-07-30 11:32:14 +0530

    initial commit

---
+++ Stack.swift
@@ -0,0 +1,59 @@
+import UIKit
+
+
+/// Naveen Dummy Duplicate Code
+
+struct Stack<T> {
+    var items: [T] = []
+    var isEmpty: Bool {
+        peek() == nil
+    }
+    
+    init() { }
+    
+    init(items: [T]) {
+        self.items = items
+    }
+    
+    mutating func push(_ element: T) {
+        items.append(element)
+    }
+    
+    mutating func pop() -> T? {
+        items.popLast()
+    }
+    
+    func peek() -> T? {
+        items.last
+    }
+}
+
+func balancedParentheses(_ s: String) -> Bool {
+    var stack: Stack<Character> = Stack<Character>()
+    
+    for char in s {
+        if char == "(" {
+            stack.push(char)
+        }
+        if char == ")" {
+            guard !stack.isEmpty else {
+                return false
+            }
+            stack.pop()
+        }
+    }
+    
+    return stack.isEmpty
+}
+
+//print(balancedParentheses("(hello world"))
+
+func reveredString(_ input: [Character]) {
+    var stack: Stack<Character> = Stack(items: input)
+    
+    while !stack.isEmpty {
+        print(stack.pop()!)
+    }
+}
+
+reveredString(["A","B","C","D"])
